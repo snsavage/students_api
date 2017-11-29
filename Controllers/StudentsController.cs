3c78b9c4c9d61eb268dd29bc51fe33e7605a57fd
Scott Savage <snsavage@gmail.com>
2017-11-29 12:42:46 -0500

    Refactor StudentControllers to remove duplicate db creation

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -11,33 +11,26 @@ namespace StudentAPI.Controllers
     [Route("api/[controller]")]
     public class StudentsController : Controller
     {
+        private StudentDb db = new StudentDb();
+
         [HttpGet]
         public IEnumerable<Student> Get()
         {
-            using (StudentDb db = new StudentDb())
-            {
-                return db.Students.ToList();
-            }
+            return db.Students.ToList();
         }
 
         [HttpGet("{id}")]
         public Student Get(int id)
         {
-            using (StudentDb db = new StudentDb())
-            {
-                return db.Students.First(t => t.Id == id);
-            }
+            return db.Students.First(t => t.Id == id);
         }
 
         [HttpPost]
         public void Post([FromBody]JObject value)
         {
             Student posted = value.ToObject<Student>();
-            using (StudentDb db = new StudentDb())
-            {
-                db.Students.Add(posted);
-                db.SaveChanges();
-            }
+            db.Students.Add(posted);
+            db.SaveChanges();
         }
 
         [HttpPut("{id}")]
@@ -45,23 +38,17 @@ namespace StudentAPI.Controllers
         {
             Student posted = value.ToObject<Student>();
             posted.Id = id;
-            using (StudentDb db = new StudentDb())
-            {
-                db.Students.Update(posted);
-                db.SaveChanges();
-            }
+            db.Students.Update(posted);
+            db.SaveChanges();
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            using (StudentDb db = new StudentDb())
+            if (db.Students.Where(t => t.Id ==id).Count() > 0)
             {
-                if (db.Students.Where(t => t.Id ==id).Count() > 0)
-                {
-                    db.Students.Remove(db.Students.First(t => t.Id == id));
-                    db.SaveChanges();
-                }
+                db.Students.Remove(db.Students.First(t => t.Id == id));
+                db.SaveChanges();
             }
         }
     }
